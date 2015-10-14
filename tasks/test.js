2f7483ffa4533217eb4adcf523480ca13e8e39f9
Adam Smith <adamsmith@algorithmiv.com>
2015-10-14 16:35:22 -0700

    Refactored make task: test

--- tasks/test.js
+++ tasks/test.js
@@ -15,51 +15,129 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// DEFINE PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/** @type {string} */
-var mocha = (
-  'node ./node_modules/mocha/bin/mocha ' +
-  '--bail '           +
-  '--colors '         +
-  '--reporter dot '   +
-  '--slow 5 '         +
-  '--timeout 1000 '
-);
-
-
-////////////////////////////////////////////////////////////////////////////////
-// DEFINE THE TASK METHODS
-////////////////////////////////////////////////////////////////////////////////
-
-/** @type {!Object<string, function>} */
-var methods = {};
-
-/** @type {function} */
-methods.all = function() {
-
-  /** @type {string} */
-  var source;
-  /** @type {string} */
-  var tests;
-
-  tests = './tests/*.js';
-  source = './src/log-ocd.js';
-
-  log.setConfig('debug.spaceAfter', 0);
-  log.debug('Testing `' + source + '`');
-
-  exec(mocha + ' ' + tests);
-
-  log.pass('Finished testing `' + source + '`');
-  log.resetConfig();
-};
-
-
-////////////////////////////////////////////////////////////////////////////////
-// EXPORT THE TASK
+// DEFINE & EXPORT THE TASK
 ////////////////////////////////////////////////////////////////////////////////
 
 /** @type {!Task} */
-module.exports = newTask('test', 'all', methods);
+module.exports = newTask('test', 'main', {
+
+  /**
+   * @param {string=} options
+   */
+  main: function main(options) {
+
+    /** @type {string} */
+    var source;
+    /** @type {string} */
+    var tests;
+
+    options = getOptions(options);
+
+    configLog();
+
+    source = './src/log-ocd.js';
+    tests = './tests/*.js';
+
+    logStart(source);
+    runTests(options, tests);
+    logFinish(source);
+
+    resetLog();
+  }
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+// DEFINE PRIVATE HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @param {string=} options
+ * @return {string}
+ */
+function getOptions(options) {
+
+  /** @type {!Object} */
+  var defaults;
+  /** @type {string} */
+  var result;
+
+  options = is.str(options) ? options.split('+') : [];
+
+  defaults = {
+    reporter: 'dot',
+    slow: 5,
+    timeout: 1000
+  };
+  result = '--colors ';
+
+  each(options, function(/** string */ option) {
+    if ( /=/.test(option) ) {
+      defaults[ getName(option) ] = getVal(option);
+    }
+    else {
+      result += option && '--' + hyphenate(option) + ' ';
+    }
+  });
+
+  each(defaults, function(/** * */ val, /** string */ option) {
+    result += '--' + hyphenate(option) + ' ' + val + ' ';
+  });
+
+  return result;
+}
+
+/**
+ * @param {string} options
+ * @param {string} tests
+ */
+function runTests(options, tests) {
+  exec('node ./node_modules/mocha/bin/mocha ' + options + ' ' + tests);
+}
+
+/**
+ * @param {string} source
+ */
+function logStart(source) {
+  log.debug('Testing `' + source + '`');
+}
+
+/**
+ * @param {string} source
+ */
+function logFinish(source) {
+  log.pass('Finished testing `' + source + '`');
+}
+
+/** @type {function} */
+function configLog() {
+  log.setConfig('pass.spaceAfter', 2);
+  log.setConfig('debug.spaceAfter', 0);
+}
+
+/** @type {function} */
+function resetLog() {
+  log.resetConfig();
+}
+
+/**
+ * @param {string} str
+ */
+function getName(str) {
+  return str && str.replace(/^([a-z]+)(?:[^a-z].*)?$/i, '$1');
+}
+
+/**
+ * @param {string} str
+ */
+function getVal(str) {
+  return str && str.replace(/^[a-z]+\=(.*)?$/i, '$1');
+}
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
+function hyphenate(str) {
+  return str && str.replace(/([A-Z])/g, '-$1').toLowerCase();
+}
