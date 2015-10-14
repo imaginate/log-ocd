4a6840c551594f54a3fce2f1727c72aa72a5be59
Adam Smith <adamsmith@algorithmiv.com>
2015-10-14 16:17:56 -0700

    Refactored make task: compile

--- tasks/compile.js
+++ tasks/compile.js
@@ -15,58 +15,70 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// DEFINE THE TASK METHODS
-////////////////////////////////////////////////////////////////////////////////
-
-/** @type {!Object<string, function>} */
-var methods = {};
-
-/** @type {function} */
-methods.all = function() {
-
-  /** @type {string} */
-  var fileIntro;
-  /** @type {string} */
-  var contents;
-  /** @type {!Array<string>} */
-  var inserts;
-  /** @type {!RegExp} */
-  var regex;
-
-  contents = retrieve.file('parts/export.js')
-    .replace(/\r\n?/g, '\n')
-    .replace(/^\/\*[\s\S]*?\*\//, '');
-
-  inserts = ('config methods helpers').split(' ');
-
-  regex = /  \/\/ INSERT ([a-z-]+\.js)\n/g;
-  each(inserts, function(/** string */ filename) {
-    filename += '.js';
-    contents = contents.replace(regex,
-      function(/** string */ match, /** string */ insertname) {
-        return ( insertname === filename ?
-          retrieve.file('parts/' + filename)
-            .replace(/\r\n?/g, '\n')
-            .replace(/^\/\*[\s\S]*?\*\/\n\n/, '')
-          : match
-        );
-      }
-    );
-  });
-
-  fileIntro = retrieve.file('src/log-ocd.js')
-    .replace(/\r\n?/g, '\n')
-    .replace(/^(\/\*[\s\S]*?\*\/\n)[\s\S]*$/, '$1');
-
-  (fileIntro + contents).to('src/log-ocd.js');
-
-  log.pass('Completed `compile.all` Task');
-};
-
-
-////////////////////////////////////////////////////////////////////////////////
-// EXPORT THE TASK
+// DEFINE & EXPORT THE TASK
 ////////////////////////////////////////////////////////////////////////////////
 
 /** @type {!Task} */
-module.exports = newTask('compile', 'all', methods);
+module.exports = newTask('compile', 'main', {
+
+  /**
+   * @type {function}
+   */
+  main: function main() {
+
+    /** @type {string} */
+    var contents;
+    /** @type {string} */
+    var parts;
+
+    contents = getFileIntro('src/log-ocd.js');
+    contents += getFile('parts/export.js');
+
+    parts = 'config methods helpers';
+
+    each(parts.split(' '), function(/** string */ filename) {
+      contents = insertFile(contents, filename);
+    });
+    contents.to('src/log-ocd.js');
+
+    log.pass('Completed `compile.main` Task');
+  }
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+// DEFINE PRIVATE HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @param {string} filepath
+ * @return {string}
+ */
+function getFile(filepath) {
+  return retrieve.file(filepath) // get file contents
+    .replace(/\r\n?/g, '\n') // normalize line breaks
+    .replace(/^\/\*[\s\S]*?\*\/\n\n/, ''); // strip intro
+}
+
+/**
+ * @param {string} contents
+ * @param {string} filename
+ * @return {string}
+ */
+function insertFile(contents, filename) {
+  filename += '.js';
+  return contents.replace(
+    new RegExp('  \\/\\/ INSERT ' + filename.replace(/\./g, '\\.') + '\\n'),
+    getFile('parts/' + filename)
+  );
+}
+
+/**
+ * @param {string} filepath
+ * @return {string}
+ */
+function getFileIntro(filepath) {
+  return retrieve.file(filepath) // get file contents
+    .replace(/\r\n?/g, '\n') // normalize line breaks
+    .replace(/^(\/\*[\s\S]*?\*\/\n)[\s\S]*$/, '$1'); // get file intro
+}
