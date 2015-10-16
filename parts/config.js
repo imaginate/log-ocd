05aa30e85d0bf9265cff5c7ec77fb31e01ee35e3
Adam Smith <adamsmith@algorithmiv.com>
2015-10-15 18:36:08 -0700

    Updated the version to initial release: 0.0.1

--- parts/config.js
+++ parts/config.js
@@ -2,7 +2,7 @@
  * -----------------------------------------------------------------------------
  * LOG-OCD CONFIG
  * -----------------------------------------------------------------------------
- * @version 0.0.0
+ * @version 0.0.1
  * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
