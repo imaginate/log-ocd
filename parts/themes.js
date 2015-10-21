277384add43f6b79cdbf6defc099392857c7b17a
Adam Smith <adamsmith@algorithmiv.com>
2015-10-21 12:14:22 -0700

    Added new section: themes

---
+++ parts/themes.js
@@ -0,0 +1,25 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD THEMES
+ * -----------------------------------------------------------------------------
+ * @version 0.0.1
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [Colors]{@link https://www.npmjs.com/package/colors}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+
+// *****************************************************************************
+// SECTION: THEMES
+// *****************************************************************************
+
+
