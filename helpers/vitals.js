9cf92d8369cd1824f885d45313ccd8a5d798a3f5
Adam Smith <adamsmith@algorithmiv.com>
2015-12-02 11:17:28 -0800

    Updated vitals helpers

--- helpers/vitals.js
+++ helpers/vitals.js
@@ -29,27 +29,22 @@ var fs = require('fs');
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
- * @param {string...} includes - The vitals sections to append to the global. If
+ * @param {...string} includes - The vitals sections to append to the global. If
  *   "all" is only given then all sections are appended.
  */
 module.exports = function Vitals(includes) {
 
   /** @type {!Array} */
   var sections;
-  /** @type {number} */
-  var len;
 
   require('./vitals/basics');
 
-  len = arguments.length;
-  sections = !len ? [] : len > 1 ?
-    slice(arguments) : includes !== 'all' ?
-      [ includes ] : getFilepaths('helpers/vitals/');
+  sections = includes === 'all' ? getVitals() : slice(arguments);
 
-  sections.forEach(function(/** string */ section) {
+  each(sections, section => {
     section = section.replace(/^([^\.]*)(?:\.js)?$/, '$1.js');
     is.file('helpers/vitals/' + section) && require('./vitals/' + section);
-  })
+  });
 };
 
 
@@ -58,34 +53,10 @@ module.exports = function Vitals(includes) {
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
- * A shortcut for Array.prototype.slice.call(obj, 0).
- * @param {Object} obj
- * @return {Array}
- */
-function slice(obj) {
-
-  /** @type {!Array} */
-  var arr;
-  /** @type {number} */
-  var i;
-
-  i = obj.length;
-  arr = i ? new Array(i) : [];
-  while (i--) {
-    arr[i] = obj[i];
-  }
-  return arr;
-}
-
-/**
- * Get all of the filepaths from a directory.
- * @param {dirpath} dirpath
+ * Get all of the filepaths from the Vitals directory.
  * @return {!Array<string>}
  */
-function getFilepaths(dirpath) {
-
-  return fs.readdirSync(dirpath)
-    .filter(function(/** string */ filepath) {
-      return is.file(dirpath + filepath);
-    });
+function getVitals() {
+  return fs.readdirSync('helpers/vitals/')
+    .filter( filepath => is.file('helpers/vitals/' + filepath) );
 }
