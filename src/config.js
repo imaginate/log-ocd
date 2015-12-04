10bc24be2e5079668d4517f47add075ab87320a9
Adam Smith <adamsmith@algorithmiv.com>
2015-12-04 13:56:01 -0800

    Separated config into smaller parts

--- src/config.js
+++ src/config.js
@@ -26,24 +26,60 @@
 /**
  * @typedef {!{
  *   __TYPE: string,
- *   argMap: ?boolean,
+ *   argMap: boolean,
  *   header: ?boolean,
- *   stack:  ?boolean,
+ *   stack:  boolean,
  *   throw:  boolean,
  *   exit:   boolean
- * }} Config
+ * }} LogConfig
  */
 
 /**
- * A factory method for Config objects.
+ * A factory method for LogConfig objects.
  * @private
  * @param {string} validKeys
  * @param {string=} trueKeys
- * @return {!Config}
+ * @return {!LogConfig}
  */
-function newConfig(validKeys, trueKeys) {
+function newLogConfig(validKeys, trueKeys) {
 
-  /** @type {!Config} */
+  /** @type {!LogConfig} */
+  var config;
+  /** @type {string} */
+  var keys;
+
+  config = newEmptyObj('Config');
+  keys  = 'argMap, stack, throw, exit';
+  config = amend(config, keys, false, 'boolean');
+  keys  = 'header';
+  each(keys, function(key) {
+    config = validKeys && has(validKeys, key)
+      ? amend(config, key, false, 'boolean')
+      : amend(config, key, null, 'null');
+  });
+  config = seal(config);
+  trueKeys && each(trueKeys, function(key) {
+    config[key] = true;
+  });
+  return config;
+}
+
+/**
+ * @typedef {!{
+ *   __TYPE: string,
+ *   throw:  boolean,
+ *   exit:   boolean
+ * }} TraceConfig
+ */
+
+/**
+ * A factory method for TraceConfig objects.
+ * @private
+ * @return {!TraceConfig}
+ */
+function newTraceConfig() {
+
+  /** @type {!TraceConfig} */
   var config;
   /** @type {string} */
   var keys;
@@ -51,7 +87,32 @@ function newConfig(validKeys, trueKeys) {
   config = newEmptyObj('Config');
   keys  = 'throw, exit';
   config = amend(config, keys, false, 'boolean');
-  keys  = 'argMap, header, stack';
+  return seal(config);
+}
+
+/**
+ * @typedef {!{
+ *   __TYPE: string,
+ *   style:  ?boolean
+ * }} PrepConfig
+ */
+
+/**
+ * A factory method for PrepConfig objects.
+ * @private
+ * @param {string} validKeys
+ * @param {string=} trueKeys
+ * @return {!PrepConfig}
+ */
+function newPrepConfig(validKeys, trueKeys) {
+
+  /** @type {!PrepConfig} */
+  var config;
+  /** @type {string} */
+  var keys;
+
+  config = newEmptyObj('Config');
+  keys  = 'style';
   each(keys, function(key) {
     config = validKeys && has(validKeys, key)
       ? amend(config, key, false, 'boolean')
@@ -66,8 +127,60 @@ function newConfig(validKeys, trueKeys) {
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// CONFIG SETUP METHODS
+// CONFIG SETUP
 ////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @type {!Object<string, function>}
+ * @const
+ */
+var CONFIG_FACTORY = freeze({
+  'toString': newPrepConfig,
+  'log':      newLogConfig,
+  'pass':     newLogConfig,
+  'error':    newLogConfig,
+  'warn':     newLogConfig,
+  'debug':    newLogConfig,
+  'fail':     newLogConfig,
+  'trace':    newTraceConfig
+});
+
+/**
+ * @private
+ * @type {!Object<string, string>}
+ * @const
+ */
+var CONFIG_VALID_KEYS = freeze({
+  'toString': 'style',
+  'log':      '',
+  'pass':     'header',
+  'error':    'header',
+  'warn':     'header',
+  'debug':    'header',
+  'fail':     '',
+  'trace':    ''
+});
+
+/**
+ * @private
+ * @type {!Object<string, string>}
+ * @const
+ */
+var CONFIG_TRUE_KEYS = freeze({
+  'toString': '',
+  'log':      '',
+  'pass':     'header',
+  'error':    'header, stack, throw',
+  'warn':     'header',
+  'debug':    'header',
+  'fail':     '',
+  'trace':    ''
+});
+
+/**
+ * @typedef {!(LogConfig|TraceConfig|PrepConfig)} Config
+ */
 
 /**
  * @private
@@ -75,15 +188,10 @@ function newConfig(validKeys, trueKeys) {
  * @return {!Config}
  */
 function getDefaultConfig(method) {
-  switch (method) {
-    case 'log':   return newConfig('argMap,header,stack');
-    case 'pass':  return newConfig('argMap,header,stack', 'header');
-    case 'error': return newConfig('argMap,header,stack', 'header,stack,throw');
-    case 'warn':  return newConfig('argMap,header,stack', 'header');
-    case 'debug': return newConfig('argMap,header,stack', 'header');
-    case 'fail':  return newConfig('argMap,header,stack');
-    case 'trace': return newConfig();
-  }
+  return CONFIG_FACTORY[method](
+    CONFIG_VALID_KEYS[method],
+    CONFIG_TRUE_KEYS[method]
+  );
 }
 
 
