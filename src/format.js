e8af42546178f1bd06ec6d7cb985b0df6794b11a
Adam Smith <adamsmith@algorithmiv.com>
2015-12-04 19:40:45 -0800

    Built the base for the TypeFormat objects

--- src/format.js
+++ src/format.js
@@ -23,6 +23,204 @@
 // FORMAT FACTORY METHODS
 ////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * @typedef {!{
+ *   __TYPE:      string,
+ *   spaceBefore: number,
+ *   spaceAfter:  number,
+ *   accentMark:  string,
+ *   lineLimit:   number
+ * }} HeaderFormat
+ */
+
+/**
+ * @private
+ * @param {Object<string, (string|number)>=} props
+ * @return {!HeaderFormat}
+ */
+function newHeaderFormat(props) {
+
+  /** @type {!HeaderFormat} */
+  var format;
+  /** @type {string} */
+  var keys;
+
+  format = newEmptyObj('HeaderFormat');
+  keys  = 'accentMark';
+  format = amend(format, keys, '', 'string');
+  keys  = 'spaceBefore, spaceAfter, lineLimit';
+  format = amend(format, keys, 0, 'number');
+  format = seal(format);
+  return props ? fuse(format, props) : format;
+}
+
+/**
+ * @typedef {!{
+ *   __TYPE:     string,
+ *   accentMark: string,
+ *   lineLimit:  number,
+ *   indent:     number,
+ *   bullet:     string
+ * }} MsgFormat
+ */
+
+/**
+ * @private
+ * @param {Object<string, (string|number)>=} props
+ * @return {!MsgFormat}
+ */
+function newMsgFormat(props) {
+
+  /** @type {!MsgFormat} */
+  var format;
+  /** @type {string} */
+  var keys;
+
+  format = newEmptyObj('MsgFormat');
+  keys  = 'accentMark, bullet';
+  format = amend(format, keys, '', 'string');
+  keys  = 'lineLimit, indent';
+  format = amend(format, keys, 0, 'number');
+  format = seal(format);
+  return props ? fuse(format, props) : format;
+}
+
+/**
+ * @typedef {!{
+ *   __TYPE:      string,
+ *   spaceBefore: number,
+ *   spaceAfter:  number,
+ *   delimiter:   string
+ * }} ArgMapFormat
+ */
+
+/**
+ * @private
+ * @param {Object<string, (string|number)>=} props
+ * @return {!ArgMapFormat}
+ */
+function newArgMapFormat(props) {
+
+  /** @type {!ArgMapFormat} */
+  var format;
+  /** @type {string} */
+  var keys;
+
+  format = newEmptyObj('ArgMapFormat');
+  keys  = 'delimiter';
+  format = amend(format, keys, '', 'string');
+  keys  = 'spaceBefore, spaceAfter';
+  format = amend(format, keys, 0, 'number');
+  format = seal(format);
+  return props ? fuse(format, props) : format;
+}
+
+/**
+ * @typedef {!{
+ *   __TYPE:   string,
+ *   brackets: string
+ * }} StringFormat
+ */
+
+/**
+ * @private
+ * @param {Object<string, string>=} props
+ * @return {!StringFormat}
+ */
+function newStringFormat(props) {
+
+  /** @type {!StringFormat} */
+  var format;
+  /** @type {string} */
+  var keys;
+
+  format = newEmptyObj('StringFormat');
+  keys  = 'brackets';
+  format = amend(format, keys, '', 'string');
+  format = seal(format);
+  return props ? fuse(format, props) : format;
+}
+
+/**
+ * @typedef {!{
+ *   __TYPE:     string,
+ *   identifier: string,
+ *   delimiter:  string,
+ *   brackets:   string,
+ *   indent:     number
+ * }} ObjectFormat
+ */
+
+/**
+ * @private
+ * @param {Object<string, (string|number)>=} props
+ * @return {!ObjectFormat}
+ */
+function newObjectFormat(props) {
+
+  /** @type {!ObjectFormat} */
+  var format;
+  /** @type {string} */
+  var keys;
+
+  format = newEmptyObj('ObjectFormat');
+  keys  = 'identifier, delimiter, brackets';
+  format = amend(format, keys, '', 'string');
+  keys  = 'indent';
+  format = amend(format, keys, 0, 'number');
+  format = seal(format);
+  return props ? fuse(format, props) : format;
+}
+
+/**
+ * @typedef {!{
+ *   __TYPE:     string,
+ *   identifier: string,
+ *   brackets:   string
+ * }} RegExpFormat
+ */
+
+/**
+ * @private
+ * @param {Object<string, string>=} props
+ * @return {!RegExpFormat}
+ */
+function newRegExpFormat(props) {
+
+  /** @type {!RegExpFormat} */
+  var format;
+  /** @type {string} */
+  var keys;
+
+  format = newEmptyObj('RegExpFormat');
+  keys  = 'identifier, brackets';
+  format = amend(format, keys, '', 'string');
+  format = seal(format);
+  return props ? fuse(format, props) : format;
+}
+
+/**
+ * @typedef {!{
+ *   __TYPE:    string,
+ *   linesBefore: ?number,
+ *   linesAfter:  ?number,
+ *   lineLimit:   number,
+ *   header:      ?HeaderFormat,
+ *   msg:         ?MsgFormat,
+ *   argMap:      !ArgMapFormat,
+ *   undefined:   string,
+ *   null:        string,
+ *   nan:         string,
+ *   string:      !StringFormat,
+ *   object:      !ObjectFormat,
+ *   function:    !ObjectFormat,
+ *   regexp:      !RegExpFormat,
+ *   array:       !ObjectFormat,
+ *   args:        !ObjectFormat,
+ *   element:     !ObjectFormat,
+ *   document:    !ObjectFormat
+ * }} TypeFormat
+ */
 
 
 
