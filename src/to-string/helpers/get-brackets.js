2e7f9879e1f6c3fe985370d1ad3c0e290f4bd8b1
Adam Smith <adamsmith@algorithmiv.com>
2015-12-08 12:20:30 -0800

    Added toString helper: getBrackets

---
+++ src/to-string/helpers/get-brackets.js
@@ -0,0 +1,34 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: GET-BRACKETS HELPER
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+/**
+ * @param {string} brackets
+ * @param {function} color
+ * @return {!Array}
+ */
+module.exports = function getBrackets(brackets, color) {
+  brackets += brackets.length > 1 ? '' : brackets;
+  return [
+    color( brackets[0] ),
+    color( brackets[1] )
+  ];
+};
