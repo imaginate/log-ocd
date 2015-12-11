883b848068d3c9102080bc1cde90994fcfabc34a
Adam Smith <adamsmith@algorithmiv.com>
2015-12-11 15:37:49 -0800

    Added log helper: getLines

---
+++ src/log/helpers/get-lines.js
@@ -0,0 +1,32 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: GET-LINES HELPER
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var fill = require('../../helpers').fill;
+
+/**
+ * @param {number} lines
+ * @return {string}
+ */
+module.exports = function getLines(lines) {
+  lines = lines > 0 ? lines : 0;
+  return fill(lines, '\n');
+};
