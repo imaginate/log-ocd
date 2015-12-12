bf2e6b9dca117f13e94d698f3f9d068d0f863e40
Adam Smith <adamsmith@algorithmiv.com>
2015-12-12 02:26:03 -0800

    Added newStack helper: getDirpath

---
+++ src/helpers/new-stack/get-dirpath.js
@@ -0,0 +1,55 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: GET-DIRPATH HELPER
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var help = require('../index');
+var cut    = help.cut;
+var freeze = help.freeze;
+var get    = help.get;
+
+/**
+ * @private
+ * @type {!RegExp}
+ * @const
+ */
+var DIR = freeze( /(?:\(|\/)[^\/]+/ );
+
+/**
+ * @private
+ * @type {!RegExp}
+ * @const
+ */
+var OPEN_PAR = freeze( /^\(/ );
+
+/**
+ * @param {string} trace
+ * @return {!Array}
+ */
+module.exports = function getDirpath(trace) {
+
+  /** @type {!Array} */
+  var dirpath;
+
+  dirpath = get(trace, DIR);
+  dirpath = cut(dirpath, -1);
+  if (dirpath.length) dirpath[0] = cut(dirpath[0], OPEN_PAR);
+  return dirpath;
+};
