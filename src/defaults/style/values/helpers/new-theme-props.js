34f94bce95d5aac20ae5e61411cab6402f32165f
Adam Smith <adamsmith@algorithmiv.com>
2015-12-13 21:32:10 -0800

    Added style value helper: newThemeProps

---
+++ src/defaults/style/values/helpers/new-theme-props.js
@@ -0,0 +1,42 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: STYLE VALUES HELPERS - NEW-THEME-PROPS
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var remap = require('../../../../helpers').remap;
+
+var newThemeProp = require('./new-theme-prop');
+var fromThemeProps = require('./from-theme-props');
+
+/**
+ * @typedef {!Object<string, ThemeProp>} ThemeProps
+ */
+
+/**
+ * @private
+ * @param {Object} props
+ * @return {!ThemeProps}
+ */
+module.exports = function newThemeProps(props) {
+  props = remap(props, function(val) {
+    return newThemeProp(val);
+  });
+  return fromThemeProps(props);
+};
