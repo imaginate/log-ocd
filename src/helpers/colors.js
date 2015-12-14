20d3e9400cd5c74c0b07b1ec3a21d08ea05f7084
Adam Smith <adamsmith@algorithmiv.com>
2015-12-13 23:49:19 -0800

    Updated colors helper with bug fixes & trace themes

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -22,28 +22,57 @@
 
 var help = require('./index');
 var is   = help.is;
+var copy = help.copy;
+var cut  = help.cut;
 var each = help.each;
 var fuse = help.fuse;
 var has  = help.has;
 
+var capFirst = require('./cap-first');
+var newTheme = require('../settings/style/new-theme');
+
 // see https://github.com/Marak/colors.js
 var colors = require('colors/safe');
 
-////////////////////////////////////////////////////////////////////////////////
-// SET THEMES
-////////////////////////////////////////////////////////////////////////////////
-
-// stacktrace themes
-colors.setTheme({
-  ostack:   'white',
-  estack: [ 'white', 'bgBlue' ]
+/**
+ * @private
+ * @type {!Object<string, function>}
+ * @const
+ */
+var BUILD = {
+  'title': buildTitleThemes,
+  'row':   buildRowThemes
 };
 
 colors.setThemes = setThemes;
+module.exports = colors;
 
-////////////////////////////////////////////////////////////////////////////////
-// SETUP METHODS
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @private
+ * @param {!Style} style
+ * @param {string} method
+ * @param {!Array} keys - The keys for each changed property.
+ */
+function setThemes(style, method, keys) {
+
+  /** @type {!Object} */
+  var themes;
+  /** @type {function} */
+  var build;
+  /** @type {!Object} */
+  var obj;
+
+  themes = {};
+  each(keys, function(key) {
+    build = has(BUILD, key) ? BUILD[key] : buildThemes;
+    obj = style[key];
+    key = method + '.' + key;
+    obj = build(key, obj);
+    themes = fuse(themes, obj);
+  });
+
+  if ( !is.empty(themes) ) colors.setTheme(themes);
+}
 
 /**
  * @private
@@ -58,13 +87,11 @@ function makeTheme(theme) {
   var keys;
 
   result = [];
-  keys = 'color, bg';
-  each(keys, function(key) {
-    theme[key] && result.push( theme[key] );
-  });
+  if (theme.color) result = fuse(result, theme.color);
+  if (theme.bg) result = fuse(result, 'bg' + capFirst(theme.bg));
   keys = 'bold, dim, hidden, inverse, italic, reset, strikethrough, underline';
   each(keys, function(key) {
-    theme[key] && result.push(key);
+    if ( theme[key] ) result = fuse(result, key);
   });
   return result;
 }
@@ -86,7 +113,8 @@ function buildThemes(name, obj) {
 
   each(obj, function(val, key) {
     if ( !is.obj(val) ) return;
-    val = buildThemes(name + key, val);
+    key = name + '.' + key;
+    val = buildThemes(key, val);
     themes = fuse(themes, val);
   });
 
@@ -96,19 +124,76 @@ function buildThemes(name, obj) {
 /**
  * @private
  * @param {string} name
- * @param {!Object} obj
+ * @param {!StackTitleTheme} theme
+ * @return {!Object}
  */
-function setThemes(name, obj) {
+function buildTitleThemes(name, theme) {
 
   /** @type {!Object} */
   var themes;
+  /** @type {!Object} */
+  var props;
 
-  themes = buildThemes(name, obj);
-  if ( !is.empty(themes) ) colors.setTheme(themes);
+  themes = {};
+  themes[name] = makeTheme(theme);
+
+  props = buildProps(theme);
+  name += '.';
+  each(theme, function(val, key) {
+    if ( !is.obj(val) ) return;
+    key = name + key;
+    val = buildProps(val, props);
+    val = newTheme(val);
+    themes[key] = makeTheme(val);
+  });
+
+  return themes;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// EXPORTS
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @private
+ * @param {string} name
+ * @param {!StackRowTheme} theme
+ * @return {!Object}
+ */
+function buildRowThemes(name, theme) {
 
-module.exports = colors;
+  /** @type {!Object} */
+  var themes;
+  /** @type {!Object} */
+  var props;
+
+  themes = {};
+  name += '.';
+  themes[name + 'odd' ] = makeTheme(theme);
+  themes[name + 'even'] = makeTheme(theme.alternate);
+
+  props = {};
+  props.odd  = buildProps(theme);
+  props.even = buildProps(theme.alternate);
+  each(theme, function(val, key) {
+    if ( !is.obj(val) ) return;
+    val = buildProps(val, props.odd);
+    val = newTheme(val);
+    themes[name + 'odd.' + key] = makeTheme(val);
+    val = buildProps(val, props.even);
+    val = newTheme(val);
+    themes[name + 'even.' + key] = makeTheme(val);
+  });
+
+  return themes;
+}
+
+/**
+ * @private
+ * @param {!Theme} theme
+ * @param {!Object=} props
+ * @return {!Object}
+ */
+function buildProps(theme, props) {
+  props = copy(props) || {};
+  each(theme, function(val, key) {
+    if ( !is.obj(val) && val ) props[key] = val;
+  });
+  return props;
+}
