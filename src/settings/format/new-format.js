a423afbd90ba6cb291bf7bc8c5893e0c391b68a8
Adam Smith <adamsmith@algorithmiv.com>
2015-12-13 22:36:52 -0800

    Added format helper: newFormat

---
+++ src/settings/format/new-format.js
@@ -0,0 +1,55 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: FORMAT NEW-FORMAT
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var help = require('../../helpers');
+var amend = help.amend;
+var each  = help.each;
+var fuse  = help.fuse;
+var seal  = help.seal;
+
+var newEmptyObj = require('../../helpers/new-empty-obj');
+
+var CATEGORIES = require('./values/categories');
+
+var newSubFormat = require('./new-sub-format');
+
+/**
+ * @param {string} category
+ * @param {Object<string, (string|number|SubFormat)>=} props
+ * @return {!Format}
+ */
+module.exports = function newFormat(category, props) {
+
+  /** @type {!Format} */
+  var format;
+  /** @type {*} */
+  var val;
+
+  props = props || null;
+  format = newEmptyObj('Format');
+  each(CATEGORIES[category], function(obj, key) {
+    val = obj.make ? newSubFormat(key) : obj.val;
+    format = amend(format, key, val, obj.type);
+  });
+  format = seal(format);
+  return fuse(format, props);
+};
