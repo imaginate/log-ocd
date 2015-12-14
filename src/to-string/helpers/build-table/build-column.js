6d8f2a1e4242dd0b38eb690624430b92914c04db
Adam Smith <adamsmith@algorithmiv.com>
2015-12-14 15:29:27 -0800

    Added buildTable helper: buildColumn

---
+++ src/to-string/helpers/build-table/build-column.js
@@ -0,0 +1,63 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: BUILD-COLUMN HELPER
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var fill = require('../../../helpers').fill;
+
+/**
+ * @typedef {!{
+ *   key:   string,
+ *   len:   number,
+ *   title: string,
+ *   align: string,
+ *   space: !Array
+ * }} Column
+ */
+
+/**
+ * @this {!Settings}
+ * @param {!Stack} stack
+ * @param {string} key
+ * @param {string} title
+ * @return {!Column}
+ */
+module.exports = function buildColumn(stack, key, title) {
+
+  /** @type {!Column} */
+  var column;
+  /** @type {!StackFormat} */
+  var format;
+
+  format = this.trace.format[key];
+  column = {
+    key:   key,
+    len:   stack[key],
+    title: title,
+    align: key === 'line' || key === 'column' ? 'right' : 'left',
+    space: [
+      fill(format.spaceBefore, ' '),
+      fill(format.spaceAfter,  ' ')
+    ]
+  };
+  if (title.length > column.len) column.len = title.length;
+  if (column.len > format.lineLimit) column.len = format.lineLimit;
+  return column;
+};
