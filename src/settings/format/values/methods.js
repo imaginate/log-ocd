87d053ec3fa78c413d72cd905d8b486c476ff6db
Adam Smith <adamsmith@algorithmiv.com>
2015-12-13 22:37:54 -0800

    Added format values: methods

---
+++ src/settings/format/values/methods.js
@@ -0,0 +1,54 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: FORMAT VALUES - METHODS
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var help = require('../../../helpers');
+var freeze = help.freeze;
+var remap  = help.remap;
+
+/**
+ * @typedef {!{
+ *   category:  string,
+ *   makeProps: ?function
+ * }} FormatDefault
+ *
+ * @typedef {!Object<string, FormatDefault>} FormatDefaults
+ */
+
+/** @type {FormatDefaults} */
+var methods = {
+  'toString': { category: 'prep'  },
+  'log':      { category: 'log'   },
+  'pass':     { category: 'log'   },
+  'error':    { category: 'log'   },
+  'warn':     { category: 'log'   },
+  'debug':    { category: 'log'   },
+  'fail':     { category: 'log'   },
+  'trace':    { category: 'trace' }
+};
+
+// append null makeProps property to each method
+methods = remap(methods, function(obj) {
+  obj.makeProps = null;
+  return obj;
+});
+
+module.exports = freeze(methods, true);
