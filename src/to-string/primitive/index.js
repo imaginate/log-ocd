5c3ea5c902b6c0883b08ee110efc76c1f7cbc575
Adam Smith <adamsmith@algorithmiv.com>
2016-01-05 14:40:26 -0800

    Added primitiveToString

---
+++ src/to-string/primitive/index.js
@@ -0,0 +1,56 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: PRIMITIVE-TO-STRING
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var help = require('../../helpers');
+var is    = help.is;
+var until = help.until;
+
+var stripStyle = require('../helpers/strip-style');
+var noStyle = require('../helpers/no-style');
+
+/**
+ * @private
+ * @type {string}
+ * @const
+ */
+var TYPES = 'string, number, boolean, undefined, null, nan';
+
+/**
+ * @this {!Settings}
+ * @param {string} method
+ * @param {*} val
+ * @return {string}
+ */
+module.exports = function primitiveToString(method, val) {
+
+  /** @type {string} */
+  var result;
+  /** @type {string} */
+  var type;
+
+  until(true, TYPES, function(typeOpt) {
+    if ( is[typeOpt](val) ) type = typeOpt;
+    return !!type;
+  });
+  result = require('./' + type).call(this, method, val);
+  return noStyle(this[method].config) ? stripStyle(result) : result;
+};
