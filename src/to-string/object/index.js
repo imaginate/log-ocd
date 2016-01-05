d7972a2009edc58767b990e1ab82863f72b0bfe3
Adam Smith <adamsmith@algorithmiv.com>
2016-01-05 13:45:36 -0800

    Created sub-dir for all objectToString methods

---
+++ src/to-string/object/index.js
@@ -0,0 +1,64 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: OBJECT-TO-STRING
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var help = require('../../helpers');
+var is    = help.is;
+var until = help.until;
+
+var stripStyle = require('../helpers/strip-style');
+var noStyle = require('../helpers/no-style');
+
+var isOcdmap = require('./helpers/ocd-map');
+
+/**
+ * @private
+ * @type {string}
+ * @const
+ */
+var TYPES = 'function, regexp, array, args, element, document, object';
+
+/**
+ * @this {!Settings}
+ * @param {string} method
+ * @param {(!Object|function)} obj
+ * @return {string}
+ */
+module.exports = function objectToString(method, obj) {
+
+  /** @type {!Config} */
+  var config;
+  /** @type {string} */
+  var result;
+  /** @type {string} */
+  var types;
+  /** @type {string} */
+  var type;
+
+  until(true, TYPES, function(typeOpt) {
+    if ( is[typeOpt](obj) ) type = typeOpt;
+    return !!type;
+  });
+  config = this[method].config;
+  type = isOcdmap(config, type, obj) ? 'ocdmap' : type;
+  result = require('./' + type).call(this, method, obj);
+  return noStyle(config) ? stripStyle(result) : result;
+};
