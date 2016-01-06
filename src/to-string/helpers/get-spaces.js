f8f710daaff43e9852cf60a9a1cdf64688bcffd2
Adam Smith <adamsmith@algorithmiv.com>
2016-01-05 16:27:42 -0800

    Added toString helper: getSpaces

---
+++ src/to-string/helpers/get-spaces.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: GET-SPACES HELPER
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var getSpace = require('./get-space');
+
+/**
+ * @param {string} spaceBefore
+ * @param {string} spaceAfter
+ * @param {string=} style
+ * @return {!Array<string>}
+ */
+module.exports = function getSpaces(spaceBefore, spaceAfter, style) {
+  return [
+    getSpace(spaceBefore, style),
+    getSpace(spaceAfter,  style)
+  ];
+};
