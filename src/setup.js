306948df6744264808aaf55ad5fce4f2bd368e3b
Adam Smith <adamsmith@algorithmiv.com>
2016-01-08 22:42:58 -0800

    Added the setup script

--- src/setup.js
+++ src/setup.js
@@ -21,19 +21,100 @@
 'use strict';
 
 var help = require('./helpers');
-var is     = help.is;
-var are    = help.are;
-var amend  = help.amend;
-var copy   = help.copy;
-var create = help.create;
-var cut    = help.cut;
-var each   = help.each;
-var fill   = help.fill;
-var freeze = help.freeze;
-var fuse   = help.fuse;
-var get    = help.get;
-var has    = help.has;
-var remap  = help.remap;
-var seal   = help.seal;
-var slice  = help.slice;
-var until  = help.until;
+var each = help.each;
+var fuse = help.fuse;
+
+var capFirst = require('./helpers/cap-first');
+
+var getDefaultSettings = require('./settings');
+var log = require('./log');
+var logTrace = require('./log/trace');
+var toString = require('./to-string');
+var set = require('./set');
+var reset = require('./reset');
+
+/** @type {number} */
+var instCount = 0;
+
+/**
+ * @typedef {!{
+ *   toString:    function,
+ *   log:         function,
+ *   pass:        function,
+ *   error:       function,
+ *   warn:        function,
+ *   debug:       function,
+ *   fail:        function,
+ *   trace:       function,
+ *   setConfig:   function,
+ *   setFormat:   function,
+ *   setStyle:    function,
+ *   resetConfig: function,
+ *   resetFormat: function,
+ *   resetStyle:  function
+ * }} LogOCD
+ */
+
+/**
+ * @return {!LogOCD}
+ */
+module.exports = function setupLogOCD() {
+
+  /** @type {!Settings} */
+  var settings;
+  /** @type {!LogOCD} */
+  var logocd;
+  /** @type {function} */
+  var setter;
+
+  settings = getDefaultSettings(++instCount);
+  logocd = bind(log, settings, 'log');
+  logocd.log = logocd;
+  each('pass, error, warn, debug, fail', function(method) {
+    logocd[method] = bind(log, settings, method);
+  });
+  logocd.trace = bind(logTrace, settings, 'trace');
+  logocd.toString = bind(toString, settings, 'toString');
+  each(logocd, function(method) {
+    method = appendSet(method, settings, method);
+  });
+  logocd = appendSet(logocd, settings);
+  return logocd;
+};
+
+/**
+ * @private
+ * @param {function} func
+ * @param {Settings} settings
+ * @param {string=} method
+ * @return {function}
+ */
+function bind(func, settings, method) {
+  return method ? func.bind(settings, method) : func.bind(settings);
+}
+
+/**
+ * @private
+ * @param {function} func
+ * @param {Settings} settings
+ * @param {string=} method
+ * @return {function}
+ */
+function appendSet(func, settings, method) {
+
+  /** @type {function} */
+  var resetter;
+  /** @type {function} */
+  var setter;
+
+  each('config, format, style', function(setting) {
+    setter = set(setting);
+    resetter = reset(setting);
+    setting = capFirst(setting);
+    setting = fuse('set', setting);
+    func[setting] = bind(setter, settings, method);
+    setting = fuse('re', setting);
+    func[setting] = bind(resetter. settings, method);
+  });
+  return func;
+}
