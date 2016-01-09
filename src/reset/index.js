21397043624fcc3f0d2bc4560bddc00d9835ee7f
Adam Smith <adamsmith@algorithmiv.com>
2016-01-08 22:42:39 -0800

    Added a reset retriever

---
+++ src/reset/index.js
@@ -0,0 +1,32 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: RESET
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var fuse = require('../helpers').fuse;
+
+/**
+ * @param {string} setting
+ * @return {function}
+ */
+module.exports = function reset(setting) {
+  setting = fuse('./', setting);
+  return require(setting);
+};
