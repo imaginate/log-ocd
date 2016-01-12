25d6547b0904f2dab9914b39ddcf8464ceacbfd1
Adam Smith <adamsmith@algorithmiv.com>
2016-01-12 13:34:32 -0800

    Added methods helper: propRangeError

---
+++ src/methods/helpers/prop-range-error.js
@@ -0,0 +1,62 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: PROP-RANGE-ERROR HELPER
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [Colors]{@link https://github.com/Marak/colors.js}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var fuse = require('../../helpers').fuse;
+var log = require('../log');
+
+/**
+ * @param {Settings} settings
+ * @param {string} method
+ * @param {string} prop
+ * @return {boolean}
+ */
+module.exports = function propRangeError(settings, method, prop) {
+
+  /** @type {string} */
+  var errorMsg;
+  /** @type {!Config} */
+  var config;
+  /** @type {string} */
+  var header;
+  /** @type {!RangeError} */
+  var error;
+  /** @type {!Array} */
+  var args;
+  /** @type {string} */
+  var msg;
+
+  config = this.error.config;
+
+  header = fuse('Invalid `log-ocd.', method, '` Call');
+  msg = fuse('the setting prop, `', prop, '`, does not exist');
+
+  errorMsg = fuse(header, ': ', msg);
+  error = new RangeError(errorMsg);
+
+  args = [ 'error' ];
+  if (config.header) args = fuse(args, header);
+  if (config.msg) args = fuse(args, msg);
+  args = fuse(args, error);
+  log.apply(settings, args);
+
+  return false;
+};
