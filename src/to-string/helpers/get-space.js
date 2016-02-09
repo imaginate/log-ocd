185a29e6eb40b7dfeaf47065e067330dd46a3b49
Adam Smith <adamsmith@algorithmiv.com>
2016-02-08 21:03:16 -0800

    Updated version to 1.0.0-beta.6

--- src/to-string/helpers/get-space.js
+++ src/to-string/helpers/get-space.js
@@ -2,7 +2,7 @@
  * -----------------------------------------------------------------------------
  * LOG-OCD: GET-SPACE HELPER
  * -----------------------------------------------------------------------------
- * @version 1.0.0-beta.5
+ * @version 1.0.0-beta.6
  * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
