5a667e00589034032d00d9b4d3b308498d71ddcb
Adam Smith <adam@imaginate.life>
2016-03-07 23:08:36 -0800

    Removed are lib from example docs

--- example/index.js
+++ example/index.js
@@ -8,7 +8,6 @@
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
  *
  * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  * @see [chalk]{@link https://github.com/chalk/chalk}
  * @see [vitals]{@link https://github.com/imaginate/vitals}
  */
