6c7099e7fb7dc3062b5902b05cd7432e68e7380d
Adam Smith <imagineadamsmith@gmail.com>
2022-12-13 01:02:05 -0800

    Added fallback for failed stackToString

---
+++ src/to-string/stack/fallback.js
@@ -0,0 +1,30 @@
+/**
+ * -----------------------------------------------------------------------------
+ * LOG-OCD: STACK-TO-STRING FALLBACK
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0-beta.10
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [chalk]{@link https://github.com/chalk/chalk}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var getStack = require('../../helpers/new-stack/get-stack');
+
+/**
+ * @param {?Error=} error
+ * @return {string}
+ */
+module.exports = function stackToStringFallback(error) {
+  return getStack(error).join('\n');
+};
